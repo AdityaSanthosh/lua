63b39fba2a6f8a93cec728a00b595af8c60e9f51
Aditya <pssanthoshaditya2000@gmail.com>
2018-12-28 18:35:39 +0530

    first commit

---
+++ hello10.lua
@@ -0,0 +1,19 @@
+for i=1,10,1 do 
+print("Hello",i)
+end
+
+--[[ for loops in lua has 2 variants generic for and numeric for
+numeric for Syntax:
+for var=exp1,exp2,exp3 do           --var=Values ranging from exp1 to exp2 incrementing by exp3            
+something
+end
+
+This command creates a lua binary coded non-executable file 
+"luac -o hellomte10.luac hellomte10.lua"
+
+It can be run from terminal normally
+
+lua hello10.luac
+
+If you try to make .luac files to executable,the terminal throws an error
+]]--
